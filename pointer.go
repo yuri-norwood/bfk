66802f14b2470849589a8de403ca2f488e808ae9
Yuri Norwood <76186754+yuri-norwood@users.noreply.github.com>
2021-03-13 01:18:55 +1300

    Create pointer.go

---
+++ pointer.go
@@ -0,0 +1,34 @@
+package bfk
+
+var (
+	pointer_max int64 = 30000
+	pointer_min int64 = 0
+)
+
+type Pointer int64
+
+func (pointer *Pointer) Increment() {
+	// Increment the value
+	value := int64(*pointer) + 1
+
+	// Check maximum not exceeded
+	if value > pointer_max {
+		value = pointer_min
+	}
+
+	// Asign new value
+	*pointer = Pointer(value)
+}
+
+func (pointer *Pointer) Decrement() {
+	// Decrement the value
+	value := int64(*pointer) - 1
+
+	// Check minimum not exceeded
+	if value < pointer_min {
+		value = pointer_max
+	}
+
+	// Asign new value
+	*pointer = Pointer(value)
+}
