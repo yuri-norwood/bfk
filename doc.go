98b08ca378dd9821682885c54971f48633b681bf
Yuri Norwood <76186754+yuri-norwood@users.noreply.github.com>
2021-03-18 22:40:21 +1300

    Use line comments instead of block as it causes the doc to fail for some reason

--- doc.go
+++ doc.go
@@ -2,17 +2,15 @@
 // Use of this source code is licensed under the Unlicense,
 // the terms of which can be found in the LICENSE file.
 
-/**
- * Package bfk provides methods and types to parse, interpret,
- * and compile, Brainfuck code.
- *
- * Parsing:
- *
- *	program := bfk.Parse("+[>>>->-[>->----<<<]>>]>.---.>+..+++.>>.<.>>---.<<<.+++.------.<-.>>+.")
- *
- * Execution:
- *
- *	var stream io.ReadWriter
- *	program.Execute(stream)
- */
+// Package bfk provides methods and types to parse, interpret,
+// and compile, Brainfuck code.
+// 
+// Parsing:
+// 
+// 	program := bfk.Parse("+[>>>->-[>->----<<<]>>]>.---.>+..+++.>>.<.>>---.<<<.+++.------.<-.>>+.")
+// 
+// Execution:
+// 
+// 	var stream io.ReadWriter
+// 	program.Execute(stream)
 package bfk
