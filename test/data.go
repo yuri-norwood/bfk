5af3069dea7b4a829cc731eb230fec18a101123e
Yuri Norwood <76186754+yuri-norwood@users.noreply.github.com>
2021-03-21 18:21:31 +1300

    Added testing code for 99 bottles of beer on the wall

---
+++ test/data.go
@@ -0,0 +1,28 @@
+package test
+
+type code struct {
+	name   string
+	source string
+	input  []byte
+	output []byte
+}
+
+func NewCode(name, source string, input, output []byte) code {
+}
+
+//go:embed _data/rdebath/testing/
+
+var (
+	//go:embed _data/rdebath/testing/Beer.b
+	beerSource string
+
+	//go:embed _data/rdebath/testing/Beer.out
+	beerOutput []byte
+
+	beer = code{
+		name:   "99 Bottles of Beer on the Wall",
+		source: beer,
+		input:  nil,
+		output: beer,
+	}
+)
