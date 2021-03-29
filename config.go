5f978b8ef0707c27e9dcd166fc9c37c30d08c106
Yuri Norwood <76186754+yuri-norwood@users.noreply.github.com>
2021-03-30 01:35:03 +1300

    Create config.go

---
+++ config.go
@@ -0,0 +1,12 @@
+// (C) 2021 Yuri Norwood
+// Use of this source code is licensed under the Unlicense,
+// the terms of which can be found in the LICENSE file.
+
+package bfk
+
+// Config defines the behaviour of a bfk Program.
+type Config struct {
+	CellMax int64
+	CellMin int64
+	TapeSize int64
+}
