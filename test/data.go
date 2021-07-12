40d5f2af928da891a85d8fa8dfe31323b588dd16
Yuri Norwood <76186754+yuri-norwood@users.noreply.github.com>
2021-07-13 09:45:22 +1200

    Now that the rdebath test code is no longer a submodule, we are free to change the path

--- test/data.go
+++ test/data.go
@@ -12,10 +12,10 @@ type code struct {
 }
 
 var (
-	//go:embed _data/rdebath/testing/Beer.b
+	//go:embed _data/rdebath/Beer.b
 	beerSource string
 
-	//go:embed _data/rdebath/testing/Beer.out
+	//go:embed _data/rdebath/Beer.out
 	beerOutput []byte
 
 	// beer is a standard "99 bottles of beer on the wall" program.
@@ -41,13 +41,13 @@ var (
 )
 
 var (
-	//go:embed _data/rdebath/testing/Collatz.b
+	//go:embed _data/rdebath/Collatz.b
 	collatzSource string
 
-	//go:embed _data/rdebath/testing/Collatz.in
+	//go:embed _data/rdebath/Collatz.in
 	collatzInput []byte
 
-	//go:embed _data/rdebath/testing/Collatz.out
+	//go:embed _data/rdebath/Collatz.out
 	collatzOutput []byte
 
 	// collatz is a program that reads numbers, one per line, and outputs
